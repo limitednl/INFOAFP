45a3ef1a188b64f5671d5c42765f4ade9d779f86
Mrveld94 <mrveld94@gmail.com>
2018-03-01 12:44:16 +0100

    Een idee voor een datastructuur

--- CircuitSimulator/src/Lib.hs
+++ CircuitSimulator/src/Lib.hs
@@ -1,6 +1,43 @@
+{-# LANGUAGE GADTs #-}
 module Lib
     ( someFunc
     ) where
 
+import Data.String
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Component = Component {unid       :: String,
+                            resistance :: Float,
+                            voltage    :: Float,
+                            current    :: Float,
+                            rOver      :: Float,
+                            vOver      :: Float,
+                            cOver      :: Float}
+    deriving Show
+
+defaultComponent :: Component
+defaultComponent = Component {unid       = "", 
+                              resistance = 0,
+                              voltage    = 0,
+                              current    = 0,
+                              rOver      = 0,
+                              vOver      = 0,
+                              cOver      = 0}
+
+components :: [Component]
+components = [defaultComponent {unid = "b1", voltage = 2},
+              defaultComponent {unid = "r1", resistance = 5}]
+
+data SPgraph a = Parallel (SPgraph a) (SPgraph a)
+               | Series (SPgraph a) (SPgraph a)
+               | Edge a
+    deriving Show
+
+testSP :: SPgraph Component
+testSP = Series (Edge (head components)) (Edge (components !! 1))
+
+-- mkrOver :: SPgraph Component -> SPgraph Component
+-- mkrOver (Edge c)       = resistance c
+-- mkrOver (Series c1 c2) = mkrOver 
