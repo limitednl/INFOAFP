4b4eff138bea2d25fb39bccfb7cbe83d0b570a52
Mrveld94 <mrveld94@gmail.com>
2018-03-01 13:35:54 +0100

    iets verder

--- CircuitSimulator/src/Lib.hs
+++ CircuitSimulator/src/Lib.hs
@@ -11,24 +11,31 @@ someFunc = putStrLn "someFunc"
 data Component = Component {unid       :: String,
                             resistance :: Float,
                             voltage    :: Float,
-                            current    :: Float,
-                            rOver      :: Float,
+                            current    :: Float}
+data Result = Result {      rOver      :: Float,
                             vOver      :: Float,
                             cOver      :: Float}
+    deriving Show
+
+data Link = Link {from :: String, to :: String}
     deriving Show
 
 defaultComponent :: Component
 defaultComponent = Component {unid       = "", 
                               resistance = 0,
                               voltage    = 0,
-                              current    = 0,
-                              rOver      = 0,
-                              vOver      = 0,
-                              cOver      = 0}
+                              current    = 0}
+defaultResult :: Result
+defaultResult = Result {rOver      = 0,
+                        vOver      = 0,
+                        cOver      = 0}
 
 components :: [Component]
-components = [defaultComponent {unid = "b1", voltage = 2},
+components = [defaultComponent {unid = "b1", voltage = 2, vOver = 2},
               defaultComponent {unid = "r1", resistance = 5}]
+
+links :: [Link]
+links = [Link {from = "b1", to = "r1"}, Link {from = "r1", to = "b1"}]
 
 data SPgraph a = Parallel (SPgraph a) (SPgraph a)
                | Series (SPgraph a) (SPgraph a)
@@ -38,6 +45,4 @@ data SPgraph a = Parallel (SPgraph a) (SPgraph a)
 testSP :: SPgraph Component
 testSP = Series (Edge (head components)) (Edge (components !! 1))
 
--- mkrOver :: SPgraph Component -> SPgraph Component
--- mkrOver (Edge c)       = resistance c
--- mkrOver (Series c1 c2) = mkrOver 
+mkrOver :: SPgraph Component -> SPgraph Result
